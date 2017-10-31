ba070ce99a05b98e7dc9730400138134957e18f3
Peter Petras <peter.petras@fathomminds.com>
2017-10-31 22:47:47 +0100

    Modifying example schemas (foo, flip)

--- examples/Clusterpoint/Models/Schema/FooSchema.php
+++ examples/Clusterpoint/Models/Schema/FooSchema.php
@@ -35,6 +35,11 @@ class FooSchema extends Schema
             'title' => [
                 'unique' => true,
                 'required' => true,
+                'default' => function () {
+                    return (isset($this->flip) && isset($this->flip->title))
+                        ? 'Title for flip: ' . $this->flip->title
+                        : 'Title for flip: Default Title';
+                },
                 'validator' => [
                     'class' => StringValidator::class,
                     'params' => [
@@ -69,6 +74,18 @@ class FooSchema extends Schema
                         ],
                     ],
                 ],
+            ],
+            'flip' => [
+                'type' => 'schema',
+                'validator' => [
+                    'class' => FlipSchema::class,
+                ]
+            ],
+            'flip2' => [
+                'type' => 'schema',
+                'validator' => [
+                    'class' => FlipSchema::class,
+                ]
             ],
         ];
     }
