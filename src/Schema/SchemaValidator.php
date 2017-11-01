7916b4616024370a4eb96b7cef149be216045320
Peter Petras <peter.petras@fathomminds.com>
2017-11-01 15:00:26 +0100

    Adding circular dependency check for schema

--- src/Schema/SchemaValidator.php
+++ src/Schema/SchemaValidator.php
@@ -36,6 +36,7 @@ class SchemaValidator
     public function validate($resource)
     {
         $this->validateResourceType($resource);
+        $this->validateSchemaDefinition(get_class($resource), $resource->schema());
         $extraneousCheck = [];
         if (!$this->allowExtraneous) {
             $extraneousCheck = $this->validateExtraneousFields($resource);
@@ -110,6 +111,52 @@ class SchemaValidator
         }
     }
 
+    private function validateSchemaDefinition($schemaName, $schemaDefinition, $schemaChain = [])
+    {
+        $schemaChain[] = $schemaName;
+        foreach ($schemaDefinition as $field => $fieldDetails)
+        {
+            if (!isset($fieldDetails['type']) || $fieldDetails['type'] !== 'schema') {
+                continue;
+            }
+            list($nestedSchemaName, $nestedSchemaDefinition) = $this->getNestedSchema($schemaName, $field, $fieldDetails);
+            if (in_array($nestedSchemaName, $schemaChain)) {
+                $schemaChain[] = $nestedSchemaName;
+                throw new RestException(
+                    'Circular dependency found in schema definition',
+                    [
+                        'schema' => array_shift($schemaChain),
+                        'chain' => $schemaChain,
+                    ]
+                );
+            }
+            $this->validateSchemaDefinition(
+                $nestedSchemaName,
+                $nestedSchemaDefinition,
+                $schemaChain
+            );
+        }
+    }
+
+    private function getNestedSchema($schemaName, $field, $fieldDetails)
+    {
+        $validatorClass = (isset($fieldDetails['validator']['class']))
+            ? $fieldDetails['validator']['class']
+            : null;
+        if ($validatorClass === null ) {
+            throw new RestException(
+                'Missing validator class for schema field',
+                [
+                    'schema' => $schemaName,
+                    'field' => $field,
+                ]
+            );
+        }
+        $nestedSchema = $validatorClass::cast((object)[]);
+        $this->objectHasSchemaMethod($nestedSchema);
+        return [$validatorClass, $nestedSchema->schema()];
+    }
+
     private function validateRequiredFields($resource)
     {
         $errors = [];
@@ -157,10 +204,10 @@ class SchemaValidator
         $fields = [];
         foreach ($resource->schema() as $fieldName => $params) {
             if (array_key_exists($paramKey, $params) && $this->isMatchedValue(
-                $checkParamValue,
-                $params[$paramKey],
-                $paramValue
-            )) {
+                    $checkParamValue,
+                    $params[$paramKey],
+                    $paramValue
+                )) {
                 $fields[$fieldName] = $params;
             }
         }
